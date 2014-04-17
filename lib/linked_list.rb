bbd0960ab92f6dde2c3f0b94ca44a3ea6c044b83
Randy Russell <ransolo@me.com>
2014-04-16 19:43:41 -0500

    here it is

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -30,66 +30,12 @@ class LinkedList
   def get(index)
     # raise IndexError if index < 0
     item = @first_item
-    # index.times do
-      # raise IndexError if item.nil?
-      # item = item.next_list_item
-    # end
+    index.times do
+      raise IndexError if item.nil?
+      item = item.next_list_item
+    end
     item.payload
   end
   #
-  # def last
-  #   item = @first_item
-  #   return item if item.nil?
-  #   until item.last?
-  #     item = item.next_list_item
-  #   end
-  #   item.payload
-  # end
   #
-  # def to_s
-  #   item = @first_item
-  #   return "| |" if item.nil?
-  #   string = "#{item.payload}"
-  #   until item.last?
-  #     item = item.next_list_item
-  #     new_load = item.payload
-  #     string = "#{string}, #{new_load}"
-  #   end
-  #   "| #{string} |"
-  # end
-  #
-  # def [](index)
-  #   get(index)
-  # end
-  #
-  # def []=(index, value)
-  #   item = @first_item
-  #   index.times do
-  #     item = item.next_list_item
-  #   end
-  #   item.payload = value
-  # end
-  #
-  # def remove(index)
-  #   return @first_item = @first_item.next_list_item if index == 0
-  #   item = @first_item
-  #   (index - 1).times do
-  #     item = item.next_list_item
-  #   end
-  #   raise IndexError if item.nil?
-  #   item.next_list_item = item.next_list_item.next_list_item
-  #   @size -= 1
-  # end
-  #
-  # def indexOf(payload)
-  #   item = @first_item
-  #   index = 0
-  #   return nil if item.nil?
-  #   until item.payload == payload
-  #     return nil if item.last?
-  #     item = item.next_list_item
-  #     index += 1
-  #   end
-  #   index
-  # end
 end
